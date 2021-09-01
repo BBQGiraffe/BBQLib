fa80943f2aa56cb44f4d58fb5e2b28579b04d538
BBQGiraffe <alexanderortizbusiness@gmail.com>
2021-08-31 19:48:18 -0500

    begin midi process

---
+++ MidiSynthesis.cs
@@ -0,0 +1,13 @@
+namespace BBQLib
+{
+    /*
+        I have absolutely no idea how sound works,
+        shout out to Javidx9 for writing an amazing tutorial
+        actually all their tutorials are really good at explaining stuff
+        https://www.youtube.com/watch?v=tgamhuQnOkM&t=66s
+    */
+    public static class MidiSynthesis
+    {
+
+    }
+}
